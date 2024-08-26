9a75e78d7985616f5606a8977681c2556137f500
정희찬 <anselmo228@naver.com>
2024-08-26 16:21:13 +0900

    initial: initial commit

---
+++ src/main/java/pitpet_be/pitpetbe/PitpetBeApplication.java
@@ -0,0 +1,13 @@
+package pitpet_be.pitpetbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitpetBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitpetBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pitpet_be/pitpetbe/PitpetBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pitpet_be.pitpetbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitpetBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
