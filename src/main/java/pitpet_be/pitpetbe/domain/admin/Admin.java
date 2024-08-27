862a73eb2dd39187368d05e968d2f448d6bfdb06
정희찬 <anselmo228@naver.com>
2024-08-27 16:10:37 +0900

    :tada: Admin Domain 생성

---
+++ src/main/java/pitpet_be/pitpetbe/domain/admin/Admin.java
@@ -0,0 +1,21 @@
+package pitpet_be.pitpetbe.domain.admin;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+public class Admin {
+
+    @Id
+    private Long id;
+
+}
