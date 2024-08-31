7dfe11ea76c2d513ac14a305c72839be9b69da33
정희찬 <anselmo228@naver.com>
2024-08-31 23:16:11 +0900

    :wrench: feat: S3 연동

--- src/main/java/fitpet_be/presentation/controller/EstimateController.java
+++ src/main/java/fitpet_be/presentation/controller/EstimateController.java
@@ -3,12 +3,17 @@ package fitpet_be.presentation.controller;
 import fitpet_be.application.dto.request.EstimateServiceRequest;
 import fitpet_be.application.service.EstimateService;
 import fitpet_be.common.ApiResponse;
+import fitpet_be.infrastructure.s3.S3Service;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/estimates")
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EstimateController {
 
     private final EstimateService estimateService;
+    private final S3Service s3Service;
     @PostMapping()
     public ApiResponse<String> createEstimate(@RequestBody EstimateServiceRequest estimateServiceRequest)
         throws IOException {
