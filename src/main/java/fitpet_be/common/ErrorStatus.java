8ce9ee83f7e0ca64c3a9e6c7d8f9bbc5f28745ce
정희찬 <anselmo228@naver.com>
2024-09-01 15:16:46 +0900

    :wrench: feat: ErrorStatus 설정

--- src/main/java/fitpet_be/common/ErrorStatus.java
+++ src/main/java/fitpet_be/common/ErrorStatus.java
@@ -14,7 +14,8 @@ public enum ErrorStatus implements BaseErrorCode {
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
     _BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증되지 않은 요청입니다."),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "접근 권한이 없습니다.");
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "접근 권한이 없습니다."),
+    _REVEIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "REVIEW404", "해당 리뷰를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
