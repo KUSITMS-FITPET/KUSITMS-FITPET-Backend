f50cbca25c9ebc2d8b0ae8136c6f3cd11f2bca1d
LEEYUJOON <123285545+L-U-Ready@users.noreply.github.com>
2024-09-01 14:22:12 +0900

    feat: Cardnews Detail 구현 (#17)
    Cardnews 썸네일 클릭 시 상세보기에 필요한 정보들 반환하는 로직 구현했습니다.
    
    Related: #16

---
+++ src/main/java/fitpet_be/application/dto/response/CardnewsDetailResponse.java
@@ -0,0 +1,28 @@
+package fitpet_be.application.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CardnewsDetailResponse {
+
+    private Long cardNewsId;
+    private String cardNewsTitle;
+    private String cardNewsContent;
+    private String image_url;
+
+    @Builder
+    public CardnewsDetailResponse(
+            Long cardNewsId, String cardNewsTitle,
+            String cardNewsContent, String image_url) {
+
+        this.cardNewsId = cardNewsId;
+        this.cardNewsTitle = cardNewsTitle;
+        this.cardNewsContent = cardNewsContent;
+        this.image_url = image_url;
+
+    }
+
+}
