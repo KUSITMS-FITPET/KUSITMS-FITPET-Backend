2c3c40bce74c19346062a61c129495121df33c84
LeeYuJoon <leeyujoon97@gmail.com>
2024-09-02 18:24:19 +0900

    fix: Faq controller 수정
    Related:

--- src/main/java/fitpet_be/presentation/controller/FaqController.java
+++ src/main/java/fitpet_be/presentation/controller/FaqController.java
@@ -11,7 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/faqs")
 public class FaqController {
