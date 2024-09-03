8f7422ac837fc90d2bfdc9573d9c53a1a5fde334
LeeYuJoon <leeyujoon97@gmail.com>
2024-09-03 23:09:54 +0900

    feat: 관리자 견적서 로직 추가
    AdminController 견적서 추출, 검색 로직 구현
    
    Related: #35

--- src/main/java/fitpet_be/presentation/controller/AdminController.java
+++ src/main/java/fitpet_be/presentation/controller/AdminController.java
@@ -2,15 +2,22 @@ package fitpet_be.presentation.controller;
 
 import ch.qos.logback.core.subst.Token;
 import fitpet_be.application.dto.request.AdminLoginRequest;
+import fitpet_be.application.dto.request.EstimateSearchRequest;
+import fitpet_be.application.dto.response.EstimateListResponse;
 import fitpet_be.application.service.AdminService;
+import fitpet_be.application.service.EstimateService;
 import fitpet_be.common.ApiResponse;
+import fitpet_be.common.PageResponse;
 import fitpet_be.domain.model.Admin;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
 
     private final AdminService adminService;
+    private final EstimateService estimateService;
 
     @Operation(summary = "Admin 로그인", description = "Admin 계정으로 로그인 합니다")
     @PostMapping("/login")
@@ -31,4 +39,39 @@ public class AdminController {
         return ApiResponse.onSuccess(token);
 
     }
+
+    @GetMapping("/estimates/desc")
+    public ApiResponse<PageResponse<EstimateListResponse>> getEstimateListDesc(
+            @RequestParam("page") int page,
+            @RequestParam(value = "size", defaultValue = "9") int size) {
+
+        Pageable pageable = PageRequest.of(page -1, size);
+
+        return ApiResponse.onSuccess(estimateService.getEstimateListDesc(pageable));
+
+    }
+
+    @GetMapping("/estimates/desc")
+    public ApiResponse<PageResponse<EstimateListResponse>> getEstimateListAsc(
+            @RequestParam("page") int page,
+            @RequestParam(value = "size", defaultValue = "9") int size) {
+
+        Pageable pageable = PageRequest.of(page -1, size);
+
+        return ApiResponse.onSuccess(estimateService.getEstimateListAsc(pageable));
+
+    }
+
+    @PostMapping("/estimates/search")
+    public ApiResponse<PageResponse<EstimateListResponse>> getEstimateListSearch(
+            @RequestBody EstimateSearchRequest request,
+            @RequestParam("page") int page,
+            @RequestParam(value = "size", defaultValue = "9") int size) {
+
+        Pageable pageable = PageRequest.of(page -1, size);
+
+        return ApiResponse.onSuccess(estimateService.getEstimateListSearch(request, pageable));
+
+    }
+
 }
