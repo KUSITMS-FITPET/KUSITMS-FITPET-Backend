05e0c7ca30cc4ff5536f5c11fb45d74345364b53
Chung Hee chan <anselmo228@naver.com>
2024-09-04 21:12:21 +0900

    [✨FEATURE] MainPage - 누적 상담건수 API  (#43)
    * :wrench: feat: MainPage 전화 카카오톡 문의 API 기능 구현
    
    * :wrench: fix: MainPage 총 문의 횟수 더하기
    
    * :wrench: fix: 권한 Response DTO 수정

--- src/main/java/fitpet_be/application/service/AdminService.java
+++ src/main/java/fitpet_be/application/service/AdminService.java
@@ -20,5 +20,4 @@ public interface AdminService {
 
     String authorizeAdmin(AdminAccessRequest adminAccessRequest);
 
-    HttpServletResponse addCookies(Admin admin, HttpServletResponse response);
 }
