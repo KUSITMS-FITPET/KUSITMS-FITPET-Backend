6e235e44792ce6f59976faa63b040d5efe1bdf7a
LEEYUJOON <leeyujoon97@gmail.com>
2024-09-08 17:22:21 +0900

    fix: Cardnews Detail Response 수정
    기존의 카드뉴스 상세보기 API를 삭제하고, 카드뉴스 조회 Response에 CardnewsContentDetail을 추가했습니다.
    
    Related: #65

--- src/main/java/fitpet_be/application/dto/response/CardnewsListResponse.java
+++ src/main/java/fitpet_be/application/dto/response/CardnewsListResponse.java
@@ -11,15 +11,18 @@ public class CardnewsListResponse {
     private Long cardNewsId;
     private String cardNewsTitle;
     private String cardNewsContent;
+    private String cardNewsContentDetail;
     private String image_url;
 
     @Builder
     public CardnewsListResponse(Long cardNewsId, String cardNewsTitle,
-                                String cardNewsContent, String image_url) {
+                                String cardNewsContent, String cardNewsContentDetail,
+                                String image_url) {
 
         this.cardNewsId = cardNewsId;
         this.cardNewsTitle = cardNewsTitle;
         this.cardNewsContent = cardNewsContent;
+        this.cardNewsContentDetail = cardNewsContentDetail;
         this.image_url = image_url;
 
     }
--- src/main/java/fitpet_be/application/service/CardnewsService.java
+++ src/main/java/fitpet_be/application/service/CardnewsService.java
@@ -1,6 +1,5 @@
 package fitpet_be.application.service;
 
-import fitpet_be.application.dto.response.CardnewsDetailResponse;
 import fitpet_be.application.dto.response.CardnewsListResponse;
 import fitpet_be.common.PageResponse;
 import org.springframework.data.domain.Pageable;
@@ -11,7 +10,5 @@ public interface CardnewsService {
     PageResponse<CardnewsListResponse> getCardnewsListDesc(Pageable pageable);
 
     PageResponse<CardnewsListResponse> getCardnewsListAsc(Pageable pageable);
-
-    CardnewsDetailResponse getCardnewsDetail(Long cardNewsId);
 
 }
